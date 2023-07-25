b0375e608d077f9db564f9590ecda5af95439348
nursultan1349 <137160897+nursultan1349@users.noreply.github.com>
2023-07-25 16:55:54 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,41 @@
+// let banka=prompt('м для мужчин и ж для женшин');
+// let banka1=(banka==='м')?'мужчина':(banka==='ж')?'женщина':'что ты такое?????!!!';
+// console.log(banka1)
+// const city=prompt('enter city name.');
+// let cost;
+
+// switch(city){
+//     case'Moscow':
+//     cost=3500
+//     alert(cost)
+//     break;
+//     case 'kazan':
+//     case 'st peterburg':
+//     cost=5000;
+//     alert(cost)
+//     break
+//     default:
+//     alert('all tickets are sold')
+// }
+// let num=prompt('enter a number');
+//        switch(num){
+//        case'0':
+//        alert(0)
+//        break;
+//        case'1':
+//        alert(1)
+//        break;
+//        case'2':
+//        case'3':
+//        alert('2,3')
+//        break;
+//        default:
+//        alert('вы страше 2 лет')
+// }
+let money=0
+let money1=1
+while(money1<=100){
+    money=money1+money;
+    money1++;
+}
+console.log(money)
